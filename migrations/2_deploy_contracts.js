8a03363fe664d814c12ae9ff340d1196eb45bd35
J.P. Aulet <aulet.jp@gmail.com>
2021-02-07 11:30:30 +0100

    Improved smart contracts of Offseth & OffsToken, improved deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,17 @@
 var OffsToken = artifacts.require("./OffsToken.sol");
 var Offseth = artifacts.require("./Offseth.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(OffsToken).then(function() {
-    return deployer.deploy(Offseth, OffsToken.address);
-  });
+module.exports = async function(deployer, _networks, accounts) {
+	await deployer.deploy(OffsToken, 'OffsToken', 'OFFS', '1000000000000000000000000000');
+	const offsToken = await OffsToken.deployed();
+
+	await deployer.deploy(Offseth, offsToken.address);
+	const offseth = await Offseth.deployed();
+
+  	const balance = offseth.balance();
+  	console.log("The Offseth balance is: " + balance.toString());
+
+  	await offseth.deposit(50, {from: accounts[1]});
+  	const balance1 = await offseth.balanceOf(accounts[1]);
+  	console.log("The user " + accounts[1] + " has deposited "+ balance1.toString());
 };
